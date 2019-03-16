14c65cfb0aacdbccdf2ceb20424f5d59a2833fc9
Hossein Amerkashi <kkashi01@gmail.com>
2019-03-16 15:36:48 -0400

    Clean up

--- appinventor/blocklyeditor/src/blockColors.js
+++ appinventor/blocklyeditor/src/blockColors.js
@@ -13,11 +13,25 @@
 'use strict';
 
 Blockly.HSV_SATURATION = 0.7;
-Blockly.CONTROL_CATEGORY_HUE = "#c88534";  // [177, 143, 53]
-Blockly.LOGIC_CATEGORY_HUE = "#54b854";  //yy [119, 171, 65]
-Blockly.MATH_CATEGORY_HUE = "#4072b5";  // [63, 113, 181]
-Blockly.TEXT_CATEGORY_HUE = "#c8366a";  // [179, 45, 94]
-Blockly.LIST_CATEGORY_HUE = "#458eef";  // [73, 166, 212]
-Blockly.COLOR_CATEGORY_HUE = "#7d7d7d";  // [125, 125, 125]
-Blockly.VARIABLE_CATEGORY_HUE = "#d0602f";  // [208, 95, 45]
-Blockly.PROCEDURE_CATEGORY_HUE = "#6e4eb8";  // [124, 83, 133]
+Blockly.CONTROL_CATEGORY_HUE = "#e09244";  // [177, 143, 53]
+Blockly.EVENT_CATEGORY_HUE = "#c88432";
+// I like below color better, but maybe change later
+// Blockly.EVENT_CATEGORY_HUE = "#d69f37";
+
+//====================
+Blockly.LOGIC_CATEGORY_HUE = "#53b853";  //yy [119, 171, 65]
+Blockly.MATH_CATEGORY_HUE = "#3F71B5";  // [63, 113, 181]
+Blockly.TEXT_CATEGORY_HUE = "#c83569";  // [179, 45, 94]
+Blockly.LIST_CATEGORY_HUE = "#448def";  // [73, 166, 212]
+Blockly.COLOR_CATEGORY_HUE = "#7D7D7D";  // [125, 125, 125]
+Blockly.VARIABLE_CATEGORY_HUE = "#D05F2D";  // [208, 95, 45]
+Blockly.PROCEDURE_CATEGORY_HUE = "#6d4db8";  // [124, 83, 133]
+//====================
+
+// Blockly.LOGIC_CATEGORY_HUE = "#54b854";  //yy [119, 171, 65]
+// Blockly.MATH_CATEGORY_HUE = "#4072b5";  // [63, 113, 181]
+// Blockly.TEXT_CATEGORY_HUE = "#c8366a";  // [179, 45, 94]
+// Blockly.LIST_CATEGORY_HUE = "#458eef";  // [73, 166, 212]
+// Blockly.COLOR_CATEGORY_HUE = "#7d7d7d";  // [125, 125, 125]
+// Blockly.VARIABLE_CATEGORY_HUE = "#d0602f";  // [208, 95, 45]
+// Blockly.PROCEDURE_CATEGORY_HUE = "#6e4eb8";  // [124, 83, 133]
